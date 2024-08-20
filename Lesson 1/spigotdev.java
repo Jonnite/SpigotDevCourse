dfaef5eea924bb2e83af568a99b3389a36392e73
Jonnite <92640192+Jonnite@users.noreply.github.com>
2024-08-20 15:16:49 +0100

    Update spigotdev.java

--- Lesson 1/spigotdev.java
+++ Lesson 1/spigotdev.java
@@ -1,1 +1,28 @@
-asd
+package me.jonnite.wardensmp;
+
+import me.jonnite.wardensmp.listener.MilkCowListener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WardenSMP extends JavaPlugin {
+
+    private static WardenSMP instance;
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new MilkCowListener(), this);
+
+    }
+
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    public static WardenSMP getInstance() {
+        return instance;
+    }
+
+}
